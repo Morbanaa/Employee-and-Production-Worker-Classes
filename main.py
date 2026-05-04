0bf0991a77c87223788549924f93b5bf1a18bff1
Morbanaa <theodorerodd@gmail.com>
2026-05-04 13:59:52 -0500

    Reworked main.py breaking up the code in main into more functions. Also added a menu on the outside of the program that calls the diffrent functions

--- main.py
+++ main.py
@@ -4,7 +4,38 @@ import os
 
 def main():
     workers = []
+    has_data = False
+    while True:
 
+        # Decide if outside loop continues
+        while True:
+            if has_data == True:
+                print("Would you like to:")
+                print("(A) Change any values:")
+                print("(B) Clear Data:")
+                print("(C) Exit Program:")
+                choice = input("Choose: ").upper()
+            else:
+                print("Would you like to:")
+                print("(A) Add data:")
+                print("(B) Exit Program:")
+                choice = input("Choose: ").upper()
+
+                match choice:
+                    case "A":
+                        clear_screen()
+                        collect_data(workers)
+                    case "B":
+                        clear_screen()
+                        break
+                    case _:
+                        clear_screen()
+                        print("Not a valid input!")
+                        continue
+        break
+       
+
+def collect_data(workers):
     counter = 0
     while True:
         counter += 1
@@ -31,11 +62,11 @@ def main():
                 print("Must be a valid none decimal number!")
 
         while True:
-           try:
+            try:
                 pay_rate = float(input(f"Enter employee pay_rate #{counter}: "))
                 clear_screen()
                 break
-           except ValueError:
+            except ValueError:
                 print("Must be a valid decimal number!")
         
         # Creates worker object and adds to list workers
@@ -52,11 +83,13 @@ def main():
     display(workers)
 
 
+
 # Accepts list of worker objects and iterates over them printing them calling the display method
 def display(workers):
     print("Name ---------- Number ---------- Shift Number ---------- Pay Rate")
     for worker in workers:
         worker.display()
+    print("__________________________________________________________________\n")
 
 
 # Resets terminal for next step
