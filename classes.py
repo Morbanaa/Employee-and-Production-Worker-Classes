15ff51175a5aa3b32f175239d506a14ee57fc93e
Morbanaa <theodorerodd@gmail.com>
2026-05-04 10:01:30 -0500

    Set up contructor with parent class and set up constructor with child class using super

--- classes.py
+++ classes.py
@@ -1,6 +1,11 @@
 class Employee():
-    pass
+    def __init__(self,name,number):
+        self.name = name
+        self.number = number
 
 
 class Production_Worker(Employee):
-    pass
+    def __init__(self,name,number,shift_num,pay_rate):
+        super().__init__(name,number)
+        self.shift_num = shift_num
+        self.pay_rate = pay_rate
