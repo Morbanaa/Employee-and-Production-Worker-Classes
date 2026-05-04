cdd7a511780f44fbdd2f82e91e26a7e507662560
Morbanaa <theodorerodd@gmail.com>
2026-05-04 12:50:29 -0500

    Added getters and setters to production worker class

--- classes.py
+++ classes.py
@@ -1,14 +1,34 @@
 class Employee():
+    # Constructor
     def __init__(self,name,number):
         self.name = name
         self.number = number
 
 
 class Production_Worker(Employee):
+    # Constructor
     def __init__(self,name,number,shift_num,pay_rate):
         super().__init__(name,number)
         self.shift_num = shift_num
         self.pay_rate = pay_rate
+    
+    # Getters
+    def get_shift_num(self):
+        return self.shift_num
 
+    # Setters
+    def set_name(self,name):
+        self.name = name
+
+    def set_number(self,number):
+        self.number = number
+
+    def set_shift_number(self,shift_number):
+        self.shift_num = shift_number
+
+    def set_pay_rate(self,pay_rate):
+        self.pay_rate = pay_rate
+
+    # Display Method
     def display(self):
         print(f"{self.name:<20}{self.number:<20}{self.shift_num:<20}${self.pay_rate:.2f}")
